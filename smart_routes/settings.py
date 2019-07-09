87a7763a93411d02047941ba7852d617cc3428b4
Shirley Nguyen <Shirley@Shirleys-MacBook-Pro.local>
2019-07-09 15:09:16 -0700

    Dynamic map and input warning for each form

--- smart_routes/settings.py
+++ smart_routes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e_3zr68_1z*^ey^2x5wbnl*2oj=g($2#pgprioy05jl4(qg3g9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
